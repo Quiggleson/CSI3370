7b1b7ee3357aa8e89228a91f0658bda437913aa2
Robert <robertquigley@oakland.edu>
2023-03-26 22:53:00 -0400

    database connection works, go to /database to test

---
+++ Popple/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Popple.Models;
+
+public partial class Account
+{
+    public int AccountId { get; set; }
+
+    public string? Email { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+}
