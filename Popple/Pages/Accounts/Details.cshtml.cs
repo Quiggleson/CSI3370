f27bc027e16040c0f59ba8f8ab6ab1d115ffc696
Robert <robertquigley@oakland.edu>
2023-03-27 13:10:17 -0400

    add CRUD pages

---
+++ Popple/Pages/Accounts/Details.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Popple.Models;
+
+namespace Popple.Pages.Accounts
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Popple.Models.PoppleContext _context;
+
+        public DetailsModel(Popple.Models.PoppleContext context)
+        {
+            _context = context;
+        }
+
+      public Account Account { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FirstOrDefaultAsync(m => m.AccountId == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Account = account;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Popple/Pages/Accounts/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Popple.Models;
+
+namespace Popple.Pages.Accounts
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Popple.Models.PoppleContext _context;
+
+        public IndexModel(Popple.Models.PoppleContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Account> Account { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Accounts != null)
+            {
+                Account = await _context.Accounts.ToListAsync();
+            }
+        }
+    }
+}
